b7ebfefb5e83587999527109a6fee6d34efc452d
Mohzu <mochzuhdi04@gmail.com>
2025-03-03 23:11:39 +0700

    Frontend : Mengerjakan Halaman Struktur

---
+++ app/Livewire/Struktur.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Struktur extends Component
+{
+    public function render()
+    {
+        return view('livewire.struktur')
+        ->layout('layouts.app');
+    }
+}
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,12 @@
 use App\Livewire\Index;
 use App\Livewire\Agenda;
 use App\Livewire\Proker;
+use App\Livewire\Struktur;
 use App\Livewire\DetailProker;
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\Divisi\Index as DivisiIndex;
 use App\Http\Livewire\Kegiatan\Index as KegiatanIndex;
-use App\Http\Livewire\Struktur\Index as StrukturIndex;
-
+// use App\Http\Livewire\Struktur\Index as StrukturIndex;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +20,6 @@ Route::get('/', Index::class)->name('index');
 Route::get('/proker', Proker::class)->name('kegiatan');
 Route::get('/agenda', Agenda::class)->name('agenda');
 Route::get('/detail-proker', DetailProker::class)->name('detail-proker');
-// Route::get('/struktur', StrukturIndex::class)->name('struktur');
+Route::get('/struktur', Struktur::class);
 // Route::get('/divisi', DivisiIndex::class)->name('divisi');
 // Route::get('/kegiatan', KegiatanIndex::class)->name('kegiatan');
